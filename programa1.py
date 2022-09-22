09fee474babeef5f8532ced03b489d7e6850b57b
ciberseguranca <tiagosousasilva@enta.pt>
2022-09-22 11:44:43 +0000

    Implementa somar

---
+++ programa1.py
@@ -0,0 +1,21 @@
+# Isto é um comentário
+
+"""
+Este programa implementa funções aritméticas
+"""
+
+
+def somar(valor1, valor2):
+    total = valor1 + valor2
+    return total
+
+# decorar if porque é um ponto de partida
+
+
+if __name__ == '__main__':
+    fator1 = 10
+    fator2 = 20
+    print(f'{fator1} mais {fator2} = {somar(fator1, fator2)}')
+
+
+
