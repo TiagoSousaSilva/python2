02a5f6c65b655c495dc80ff1b5ec2fed0862e60e
ciberseguranca <tiagosousasilva@enta.pt>
2022-09-23 09:04:14 +0000

    Implementa varias operações aritemeticas

--- programa1.py
+++ programa1.py
@@ -26,13 +26,18 @@ def aritmetica(valor1, valor2, op='+'):
     return total
 
 # decorar if porque é um ponto de partida
+# Int usado para numeros inteiros, str para numeros com letras e float para numeros com decimais
 
 
 if __name__ == '__main__':
-    fator1 = 10
-    fator2 = 20
-    operacao = '-'
-    print(f'{fator1} {operacao} {fator2} = {aritmetica(fator1, fator2, operacao)}')
-
-
+    nome = input('Como te chamas? ')
+    while True:
+        fator1 = float(input('Insira o primeiro número: '))
+        fator2 = float(input('Insira o segundo número: '))
+        operacao = input('Insira a operação [+ , - , : , *]: ')
+        print(f'Olá {nome}, {fator1} {operacao} {fator2} = {aritmetica(fator1, fator2, operacao)}')
+        continuar = input('Quer contunuar? [S, N]')
+        if continuar == 'N':
+            break
+    print(f'Adeus {nome}!')
 
