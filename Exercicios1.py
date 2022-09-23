c4bbc53b93c7c4599008ce12f9dce93134e42700
ciberseguranca <tiagosousasilva@enta.pt>
2022-09-23 11:52:24 +0000

    exercicios

---
+++ Exercicios1.py
@@ -0,0 +1,11 @@
+"""
+1.Pede ao utilizador 2 numeros. O 2º numero deve ser >= 1º
+Mostra todos os numeros primos
+depois de mostrar os numeros diz quantos numeros primos havia
+
+2.Pede ao utilizador um numero inicial
+pede ao utilizador um numero que representa "quantos"
+Mostra aquela quantidade de numeros primos a partir do numero inicial
+"""
+
+def primos(num):
