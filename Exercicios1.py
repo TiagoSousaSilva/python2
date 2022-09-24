c47b7e7c4a84aa55906461e3456c5b6b02a6ca7f
Tiago Silva <tiagosousasilva@enta.pt>
2022-09-24 21:36:23 +0000

    exercicio

--- Exercicios1.py
+++ Exercicios1.py
@@ -11,22 +11,17 @@ Mostra aquela quantidade de numeros primos a partir do numero inicial
 """
 
 def primos(num1, num2, op):
-        primos = 0
-        numero = range
-        for n in range(num1, num2 + 1):
-            op = range % n
-            if op == 2:
-                primos += 1  # zeros = zeros + 1
-        return primos
+        for primos in range(num1, num2 + 1):
+            if primos > 1:
+                for n in range (2, primos):
+                    if (primos % n) == 0:
+                        break
+                else:
+                    print(primos)
 
 
 while True:
     Num1 = int(input('Insira o 1ºnum: '))
     Num2 = int(input('Insira o 2ºnum: '))
-    lista = range
-    if primos(range) == 2:
-        print(f'O numero tem {lista}')
-    else:
-        print('Não há primos')
-    print(f'Tem {primos}')
+    print(f'Os numeros primos entre {Num1} e {Num2} sao {primos}')
 
