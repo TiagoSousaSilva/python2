05b35d4647e85404f8b0b304cf64512fee720f4c
ciberseguranca <tiagosousasilva@enta.pt>
2022-09-29 11:31:10 +0000

    Exe. Listas

--- Linhas.exe2.py
+++ Linhas.exe2.py
@@ -8,6 +8,14 @@ imprimir 7 linhas total gasolina, gasolio, ilhas
         [0, 0, 0, 0, 0]
     ]
 
+vendas = [
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+]
+
 """
 """
 Minha resoluçao
@@ -29,6 +37,8 @@ vendas = [
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0]
     ]
+
+totais = [0, 0, 0, 0, 0]
 
 if __name__ == '__main__':
     for x in range(len(vendas)):
@@ -60,5 +70,50 @@ if __name__ == '__main__':
         for x in range(len(vendas)):
             total += vendas[x][y]
         print(f'Total de vendas para {ilhas[y]} = {total}')
+        totais[y] = total
 
+    # total de vendas
+
+    total= 0
+    for x in range(len(vendas)):
+        for y in range(len(vendas[x])):
+            total += vendas[x][y]
+    print(f'Total de vendas= {total}')
+
+
+    # total de vendas de combustivel, onde se vendeu menos/mais
+
+
+    maior = totais[0]
+    menor = totais[0]
+    for x in range(1, len(totais)):
+        if totais[x] > maior:
+            maior = totais[x]
+        if totais[x] < menor:
+            menor = totais[x]
+#verificaçao    print(f'O numero maior é {maior} e o numero menor é {menor}')
+
+    ilha_menor = []
+    ilha_maior = []
+
+    for x in range(len(totais)):
+        if totais[x] == maior:
+            ilha_maior.append(ilhas[x])
+        if totais[x] == menor:
+            ilha_menor.append(ilhas[x])
+
+    print(totais)
+    print(f'Ilha maior = {ilha_maior} = {maior} \nIlha menor = {ilha_menor} = {menor}')
+
+#qual ilha consumiu Mais gasoleo e mais gasolina (nao os dois juntos)
+"""
+    venda_maior = 0
+    venda_menor = 0
+
+    for y in range(len(vendas[x])):
+        for x in range(len(vendas)):
+            if vendas[x] > venda_maior:
+                venda_maior = vendas[x]
+        print(venda_maior)
+"""
 
