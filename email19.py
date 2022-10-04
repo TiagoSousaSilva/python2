88f059e81487b9e2fcedfb2ec2c05dfcc0d9daf2
ciberseguranca <tiagosousasilva@enta.pt>
2022-10-04 10:13:16 +0000

    exercicio email

---
+++ email19.py
@@ -0,0 +1,10 @@
+"""
+Escreva um programa em Python que lê uma quantia em Euros e calcula
+o número de notas de 50 e, 20 e, 10 e, 5 e e moedas de 2 e, 1 e, 50
+cêntimos, 20 cêntimos, 10 cêntimos, 5 cêntimos, 2 cêntimos e 1 cêntimo,
+
+necessário para perfazer, essa quantia, utilizando sempre o máximo nú-
+mero de notas e moedas para cada quantia, da mais elevada, para a mais
+
+baixa.
+"""
---
+++ email20.py
@@ -0,0 +1,14 @@
+"""
+Escreva um programa em Python escreve o seguinte:
+1x8+1=9
+12 x 8 + 2 = 98
+123 x 8 + 3 = 987
+1234 x 8 + 4 = 9876
+12345 x 8 + 5 = 98765
+123456 x 8 + 6 = 987654
+1234567 x 8 + 7 = 9876543
+12345678 x 8 + 8 = 98765432
+123456789 x 8 + 9 = 987654321
+Os valores do primeiro termo da multiplicação e o resultado devem ser
+calculados pelo seu programa.
+"""
